9aa6f112918d80ac6152a8806f928263a69dd321
clutchking_asiimov <saureyj43@gmail.com>
2025-03-09 23:54:32 +0100

    Added new plugins and config files

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,36 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  { 'mfussenegger/nvim-dap' }, --Debugger plugin
+  { 'Bekaboo/deadcolumn.nvim' }, --Shows deadline to manage code length
+  { 'rcarriga/nvim-dap-ui' }, --UI for DAP debugger plugin
+  { 'akinsho/toggleterm.nvim' }, --Terminal toggle & split plugin
+  { 'rmagatti/auto-session' }, --Session persistence & management
+  { 'NeogitOrg/neogit' }, -- Visually rich experience of Git inside Nvim
+
+  --Breadcrumbs + Full IDE experience in Nvim
+  {
+    'nvimdev/lspsaga.nvim',
+    after = 'nvim-lspconfig',
+    config = function()
+      require('lspsaga').setup {}
+    end,
+  },
+
+  {
+    'Bekaboo/dropbar.nvim',
+    --Parameters for the plugin
+    dependencies = {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+    },
+    config = function()
+      --Keymaps for Dropbar
+      local dropbar_api = require 'dropbar.api'
+      vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+      vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to the start of the current context' })
+      vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+    end,
+  },
+}
--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,12 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        python = { 'pylint' },
+        text = { 'vale' },
+        json = { 'jsonlint' },
+        dockerfile = { 'hadolint' },
+        bash = { 'bash' },
+        sql = { 'sqlfluff' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
